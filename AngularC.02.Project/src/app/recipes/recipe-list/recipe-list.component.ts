a9d630619b48cb3ecadd5c5f8a7fe59209a235b3
zyrab <zurakrua@gmail.com>
2023-06-20 15:11:33 +0200

    adding-styiling

--- AngularC.02.Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ AngularC.02.Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe ('A test Recipe' , 'This is a simple Test' , 'https://gastronomia.ge/wp-content/uploads/2019/04/0bb44d60c446d13af892245c2311d15d.jpg' )
+  ];
+
 
 }
