21d35782cd350f792afcae5dd137ae3fec974bda
zyrab <zurakrua@gmail.com>
2023-06-21 10:07:41 +0200

    adding-bootstrap

--- AngularC.02.Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ AngularC.02.Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe ('A test Recipe' , 'This is a simple Test' , 'https://gastronomia.ge/wp-content/uploads/2019/04/0bb44d60c446d13af892245c2311d15d.jpg' )
+    new Recipe ('Khachapuri' , 'Khachapuri is Gergian national dish. this is megrelian khachapuri.' , 'https://gastronomia.ge/wp-content/uploads/2019/04/0bb44d60c446d13af892245c2311d15d.jpg' ),
+    new Recipe ('Khinkali' , 'Khinkali is national dish of Georgia. this is qalaquri.' , 'https://images.bolt.eu/store/2022/2022-06-09/33d5f5ee-db9c-40ee-ae1e-43c928c1527f.jpeg' )
   ];
 
 
